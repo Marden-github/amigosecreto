ddf7e565538112262cd639c1751dd666450d9f5c
Marden-github <mar988681164@gmail.com>
2024-09-06 20:09:42 -0300

    desafio marden adilson

---
+++ backend/amigosecreto-service/api-gateway/src/routes/url.js
@@ -0,0 +1,8 @@
+module.exports = {
+    URL_GRUPO_API: 'http://localhost:3331',
+    URL_CONVITE_API: 'http://localhost:3332',
+    URL_LISTADESEJO_API: 'http://localhost:3333',
+    URL_LOGIN_API: 'http://localhost:3334',
+    URL_PARTICIPANTE_API: 'http://localhost:3335',
+    URL_SORTEIO_API: 'http://localhost:3336'
+  };
